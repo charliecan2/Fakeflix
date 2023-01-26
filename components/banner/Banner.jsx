0e23e8fdbabfe64fb211a57cad664d5341037bf1
charliecan2 <carloscq920@gmail.com>
2023-01-26 17:28:11 -0600

    Added a button to the Banner component, only console logs for now

--- components/banner/Banner.jsx
+++ components/banner/Banner.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 function Banner({title, subtitle, imgUrl}) {
+
+  const handleOnPlay = () => {
+    console.log("Handle on Play")
+  }
+    
   return (
     <div>
         <h3>{title}</h3>
         <h3>{subtitle}</h3>
+        <button onClick={handleOnPlay} >Play</button>
         <div style={{
             backgroundImage: `url(${imgUrl})`,
             width: "100%",
